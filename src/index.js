8b671d467c7ff8fc8cba77635f51cb40536453ec
Anton Golovko <golovko639@gmail.com>
2019-03-23 23:36:24 +0300

    Create exchange function which return the solution of makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	return exchange(currency);
+};
+
+function exchange(money) {
+	let result = {};
+
+	if (money < 1) {
+		return result;
+	}
+	else if (money > 10000) {
+		result.error = `You are rich, my friend! We don't have so much coins for exchange`;
+		return result;
+	}
+
+	let coins = [50, 25, 10, 5, 1];
+	let title = [`H`, `Q`, `D`, `N`, `P`];
+
+
+	for (let i = 0; i < coins.length; i++) {
+		let count = 0;
+
+		while (money >= coins[i]) {
+			count = Math.floor(money / coins[i]);
+			money -=  count * coins[i];
+			if (!result[title[i]]) {
+				result[title[i]] = count;
+				continue;
+			}
+
+			result[title[i]] += money;
+		}
+	}
+
+	return result;
 }
