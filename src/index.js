3ab9c905c54f2ef95d23cb2a6f51c0633f018ff6
Anton Golovko <golovko639@gmail.com>
2019-03-23 23:44:59 +0300

    Add comments to code.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ module.exports = function makeExchange(currency) {
 function exchange(money) {
 	let result = {};
 
+	// if money < 1 or money > 10000 return
 	if (money < 1) {
 		return result;
 	}
@@ -13,16 +14,21 @@ function exchange(money) {
 		return result;
 	}
 
+	// create arrays with coins value and coins title
 	let coins = [50, 25, 10, 5, 1];
 	let title = [`H`, `Q`, `D`, `N`, `P`];
 
 
 	for (let i = 0; i < coins.length; i++) {
+		// count coins[i] in money
 		let count = 0;
 
 		while (money >= coins[i]) {
+			// divide the money by the coin[i]
 			count = Math.floor(money / coins[i]);
+			// compute money after divide
 			money -=  count * coins[i];
+			// if result obj doesn't have title[i] create, else add count to title[i] in obj
 			if (!result[title[i]]) {
 				result[title[i]] = count;
 				continue;
